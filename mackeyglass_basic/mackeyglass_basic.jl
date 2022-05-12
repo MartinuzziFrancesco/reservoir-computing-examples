e7d89527c28ef314940bdbc85cd3a9040a5911ad
Francesco Martinuzzi <martinuzzi.francesco@gmail.com>
2022-05-12 17:48:47 +0200

    Update mackeyglass_basic.jl

--- mackeyglass_basic/mackeyglass_basic.jl
+++ mackeyglass_basic/mackeyglass_basic.jl
@@ -4,7 +4,7 @@ using Downloads
 
 data_path = Downloads.download("https://mantas.info/wp/wp-content/uploads/simple_esn/MackeyGlass_t17.txt", 
     string(pwd(),"/MackeyGlass_t17.txt"))
-data = reduce(hcat,readdlm(data_path))
+data = CuArray(reduce(hcat, convert(Matrix{Float32}, readdlm(data_path, ','))))
 
 washout      = 100
 train_len    = 2000
@@ -15,23 +15,19 @@ target_data  = data[:, washout+2:train_len+1]
 test_data    = data[:, train_len+2:train_len+predict_len+1]
 
 Random.seed!(42)
-W    = rand(1000, 1000) .- 0.5 
-rhoW = maximum(abs.(eigvals(W)))
-W  .*= (1.25 / rhoW)
-Win  = (rand(1000, 2) .- 0.5) .* 1
 
 esn = ESN(input_data; 
     variation = Default(),
-    reservoir = W,
-    input_layer = Win,
+    reservoir = RandSparseReservoir(1000, 1.25, 1.0),
+    input_layer = WeightedLayer(1.0),
     reservoir_driver = RNN(leaky_coefficient=0.3),
     nla_type = NLADefault(),
     states_type = PaddedExtendedStates(),
     washout=washout)
 
 training_method = StandardRidge(1e-8) 
-output_layer    = train(esn, target_data, training_method)    
-output          = esn(Generative(predict_len), output_layer)
+output_layer = train(esn, target_data, training_method)    
+output = esn(Generative(predict_len), output_layer)
 
 
 println(sum(abs2.(test_data[:,1:500] .- output[:,1:500]))/500)
